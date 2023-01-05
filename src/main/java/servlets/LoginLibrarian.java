4c40ce73635c2a2634c7c8d00ae69c2e49769126
Aleksandar Mladenovic <alex.mladenovic2000@gmail.com>
2023-01-05 21:58:49 +0200

    [Backend] Added 2 new servlets to login student and librarian

--- src/main/java/servlets/LoginLibrarian.java
+++ src/main/java/servlets/LoginLibrarian.java
@@ -5,25 +5,23 @@
 package servlets;
 
 import database.tables.EditLibrarianTable;
-import database.tables.EditStudentsTable;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import javax.servlet.http.HttpSession;
 import mainClasses.JSON_Converter;
-
-import mainClasses.Student;
 import mainClasses.Librarian;
 
 /**
  *
- * @author root
+ * @author aleks
  */
-public class Login extends HttpServlet {
+@WebServlet(name = "LoginLibrarian", urlPatterns = {"/LoginLibrarian"})
+public class LoginLibrarian extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,15 +35,15 @@ public class Login extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 //        response.setContentType("text/html;charset=UTF-8");
-//        try ( PrintWriter out = response.getWriter()) {
+//        try (PrintWriter out = response.getWriter()) {
 //            /* TODO output your page here. You may use following sample code. */
 //            out.println("<!DOCTYPE html>");
 //            out.println("<html>");
 //            out.println("<head>");
-//            out.println("<title>Servlet Login</title>");            
+//            out.println("<title>Servlet LoginLibrarian</title>");
 //            out.println("</head>");
 //            out.println("<body>");
-//            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
+//            out.println("<h1>Servlet LoginLibrarian at " + request.getContextPath() + "</h1>");
 //            out.println("</body>");
 //            out.println("</html>");
 //        }
@@ -64,25 +62,21 @@ public class Login extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
-        if(session.getAttribute("loggedIn")!=null){
+        if (session.getAttribute("loggedIn") != null) {
             response.setStatus(200);
-            EditStudentsTable eut = new EditStudentsTable();
             EditLibrarianTable lib_table = new EditLibrarianTable();
             String username = (String) session.getAttribute("loggedIn");
-            try{
-                Student s=eut.StudentUserNameExists(username);
+            try {
                 Librarian libr = lib_table.LibrarianUserNameExists(username);
-                if(s!=null){
-                    response.getWriter().write(s.getUsername());
-                } else if( libr !=null){
+                if (libr != null) {
                     response.getWriter().write(libr.getUsername());
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.err.println("Got an exception while getting loggedIn user.");
                 System.err.println(e.getMessage());
                 response.setStatus(500);
             }
-        }else{
+        } else {
             response.setStatus(403);
         }
     }
@@ -98,31 +92,28 @@ public class Login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         HttpSession session = request.getSession(true);
-        
+
         JSON_Converter jc = new JSON_Converter();
-        
-        EditStudentsTable eut = new EditStudentsTable();
+
         EditLibrarianTable lib_table = new EditLibrarianTable();
-        
-        
+
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        
-        try{
-            Student s=eut.databaseToStudent(username, password);
-            Librarian libr = lib_table.databaseToLibrarian(username,password);
-            if(libr == null && s == null){
+
+        try {
+            Librarian libr = lib_table.databaseToLibrarian(username, password);
+            if (libr == null) {
                 response.setStatus(403);
-            }else{
-                session.setAttribute("loggedIn",username);
+            } else {
+                session.setAttribute("loggedIn", username);
+                session.setAttribute("type", "librarian");
                 response.setStatus(200);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.err.println("Got an exception while Logining a user ");
             System.err.println(e.getMessage());
         }
